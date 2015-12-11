6a8d546e41bf844495c72afb6b04350e5fc08ff6
David Moody <moody.david@gmail.com>
2015-12-11 11:48:43 -0500

    finished section 4 making the dropdown

--- src/button.jsx
+++ src/button.jsx
@@ -2,8 +2,9 @@ var React = require('react');
 
 
 var Badge = React.createClass({
+
   render: function() {
-    return <button className={"btn " + this.props.className} type="button">
+    return <button onClick={this.props.onClick} className={"btn " + this.props.className} type="button">
       {this.props.title}
       <span className={this.props.subTitleClassName} >{this.props.subTitle}</span>
     </button>
--- src/dropdown.jsx
+++ src/dropdown.jsx
@@ -3,15 +3,45 @@
 
 var React = require('react');
 var Button = require('./button');
-
-var List = require('./list');
+var ListItem = require('./list-item');
 
 
 module.exports = React.createClass({
+
+  handleClick: function() {
+    this.setState({open: !this.state.open });
+
+  },
+  getInitialState: function() {
+    return { open: false}
+  },
+  handleItemClick: function(item) {
+    console.log(item + ' was clicked')
+    this.setState({
+      open: false,
+      itemTitle: item
+    });
+
+  },
   render: function() {
+
+    var list = this.props.items.map(function(itemVal) {
+      return <ListItem
+        item={itemVal}
+        whenClicked={this.handleItemClick}
+        className={(this.state.itemTitle === itemVal) ? "active" : "" }
+      />
+    }.bind(this));
+
     return <div className="dropdown">
-      <Button className="btn-default" title={this.props.title}  subTitleClassName="caret" />
+      <Button onClick={this.handleClick} className="btn-default" title={this.state.itemTitle || this.props.title}  subTitleClassName="caret" />
+
+      <ul className={"dropdown-menu " + (this.state.open ? "show" : "")} >
+        {list}
+      </ul>
       </div>
   }
 });
+
+
 
